9f9f06920fd7cb8d71aa70074b73e33783712264
alexandru <alex@hackd.net>
2011-08-12 13:28:08 -0700

    copy convergence-createdb with the rest of the scripts during setup

--- server/setup.py
+++ server/setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup, Extension
 
 shutil.copyfile("convergence-notary.py", "convergence/convergence-notary")
 shutil.copyfile("convergence-gencert.py", "convergence/convergence-gencert")
+shutil.copyfile("convergence-createdb.py", "convergence/convergence-createdb")
 shutil.copyfile("convergence-bundle.py", "convergence/convergence-bundle")
 
 setup  (name        = 'convergence-notary',
@@ -14,7 +15,7 @@ setup  (name        = 'convergence-notary',
         license = 'GPL',
         packages  = ["convergence"],
         package_dir = {'convergence' : 'convergence/'},
-        scripts = ['convergence/convergence-notary', 'convergence/convergence-gencert', 'convergence/convergence-bundle'],
+        scripts = ['convergence/convergence-notary', 'convergence/convergence-gencert', 'convergence/convergence-createdb', 'convergence/convergence-bundle'],
         data_files = [('share/convergence', ['README', 'INSTALL', 'COPYING']),
                       ('/etc/init.d', ['init-script/convergence'])]
        )
@@ -30,6 +31,7 @@ except:
 try:
     os.remove("convergence/convergence-notary")
     os.remove("convergence/convergence-bundle")
+    os.remove('convergence/convergence-createdb')
     os.remove("convergence/convergence-gencert")
 
 except:

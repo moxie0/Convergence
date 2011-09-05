352d375193ca0ad49044d182b9ef39cad39c98a3
Ewan Marshall <ewanm89@gmail.com>
2011-09-05 13:38:11 +0100

    Added setting permissions of the database file.

--- server/convergence-createdb.py
+++ server/convergence-createdb.py
@@ -43,23 +43,63 @@ USA
 """
 
 from sqlite3 import *
-import sys, os.path, os
+import sys, os.path, os, getopt, pwd, grp
 
-def main(argv):
+def main(argv):	
+    uname             = "nobody"
+    gname             = "nogroup"
+
+   try:
+        opts, args = getopt.getopt(argv, "u:g:h")
+		
+		for opt, arg in opts:
+			if opt in("-u"):
+                uname = arg
+			elif opt in("g"):
+				gname = arg
+            elif opt in ("-h"):
+                usage()
+                sys.exit()
 
     convergencePath = "/var/lib/convergence/"
     convergencedb   = convergencePath + "convergence.db"
 
     if not os.path.exists(convergencePath):
         os.makedirs(convergencePath)
-        
+		
+	
     connection = connect(convergencedb)
     cursor     = connection.cursor()
 
     cursor.execute("CREATE TABLE fingerprints (id integer primary key, location TEXT, fingerprint TEXT, timestamp_start INTEGER, timestamp_finish INTEGER)")
     connection.commit()
     cursor.close()
+	
+	setPerms(convergencedb, uname, gname)
+	
+def setPerms(path, uname, gname):
+    try:
+        user = pwd.getpwnam(uname)
+    except KeyError:
+        print >> sys.stderr, 'User ' + uname + ' does not exist, cannot set permission'
+        sys.exit(2)
+    try:
+        group = grp.getgrnam(gname)
+    except KeyError:	
+        print >> sys.stderr, 'Group ' + gname + ' does not exist, cannot set permissions'
+        sys.exit(2)
+	
+	os.chown(path,nobody.user_uid,group.gr_gid);
 
+def usage():
+    print "\nConvergence create notary database script.\n"
+    print "usage: convergence-createdb <options>\n"
+    print "Options:"
+    print "-u <username>  Name of user to set database file permissions to (optional, defaults to 'nobody')"
+    print "-g <group>     Name of group to set database file permissions to (optional, defaults to 'nogroup')"
+    print "-h             Print this help message."
+    print ""
 
+	
 if __name__ == '__main__':
     main(sys.argv[1:])

a8933e0f587b60d2f7d446ae06329f6b83e7a458
Moxie Marlinspike <moxie@thoughtcrime.org>
2011-09-11 17:52:12 -0400

    Minor refactoring.

--- client/chrome/content/options.js
+++ client/chrome/content/options.js
@@ -28,7 +28,10 @@ function onOptionsLoad() {
   convergence     = Components.classes['@thoughtcrime.org/convergence;1'].getService().wrappedJSObject;
   settingsManager = convergence.getSettingsManager();
   notaries        = settingsManager.getNotaryList();
-  update();
+
+  updateAdvancedSettings();
+  updateNotarySettings();
+  updateCacheSettings();
 }
 
 function onOptionsSave() {
@@ -47,18 +50,16 @@ function onOptionsSave() {
 function onRemoveNotary() {
   var tree = getNotaryTree();
   notaries.splice(tree.currentIndex, 1);
-  // notaryManager.removeNotaryAtIndex(tree.currentIndex);
-  update();
+  updateNotarySettings();
 }
 
 function onEditNotary() {
   var tree   = getNotaryTree();
-  // var notary  = notaryManager.getNotaryAtIndex(tree.currentIndex);
   var notary = notaries[tree.currentIndex];
   var retVal = {notary: notary};
 
   window.openDialog("chrome://convergence/content/addEditNotary.xul", "dialog2", "modal", retVal);
-  update();
+  updateNotarySettings();
 }
 
 function onAddNotary() {
@@ -67,33 +68,61 @@ function onAddNotary() {
 
   if (retVal.notary) {
     notaries.push(retVal.notary);
-    // notaryManager.addNotary(retVal.notary);
-    update();
+    updateNotarySettings();
   }
 }
 
-function update() {
+function updateAdvancedSettings() {
   var cacheCertificatesEnabled     = convergence.getSettingsManager().getCacheCertificates();
   var notaryBounceEnabled          = convergence.getSettingsManager().getNotaryBounce();
   var connectivityIsFailureEnabled = convergence.getSettingsManager().getConnectivityErrorIsFailure();
   var verificationThreshold        = convergence.getSettingsManager().getVerificationThreshold();
 
-  dump("threshold: " + verificationThreshold + "\n");
-  dump("Currently selected: " + document.getElementById("threshold").selectedItem + "\n");
-
   document.getElementById("cache-certificates").checked   = cacheCertificatesEnabled;
   document.getElementById("notary-bounce").checked        = notaryBounceEnabled;
   document.getElementById("connectivity-failure").checked = connectivityIsFailureEnabled;
   document.getElementById("threshold").selectedItem       = document.getElementById(verificationThreshold);
+};
 
+function updateCacheSettings() {
+  var certificateCache = convergence.getNativeCertificateCache();
+  cachedCerts          = certificateCache.fetchAll();
+  certificateCache.close();
+
+  var cacheTree = document.getElementById("cacheTree");
+
+  cacheTree.view = {
+    rowCount: cachedCerts.length,
+
+    getCellText : function(row, column) {
+      var cachedCert = cachedCerts[row];
+
+      if      (column.id == "cacheLocation")    return cachedCert.location;
+      else if (column.id == "cacheFingerprint") return cachedCert.fingerprint;
+      else if (column.id == "cacheTimestamp")   return formatDate(cachedCert.timestamp);
+    },
+
+    setTree: function(treebox){this.treebox = treebox; },
+    isContainer: function(row){return false;},
+    isSeparator: function(row){ return false; },
+    isSorted: function(){ return false; },
+    isEditable: function(row, column) {return false;},
+    getLevel: function(row){ return 0; },
+    getImageSrc: function(row,col){ return null; },
+    getRowProperties: function(row,props){},
+    getCellProperties: function(row,col,props){},
+    getColumnProperties: function(colid,col,props){}
+  };
+};
+
+function updateNotarySettings() {
   var notaryTree = getNotaryTree();
 
   notaryTree.view = {  
-    rowCount : notaries.length, //notaryManager.getNotaryCount(),  
+    rowCount : notaries.length,
     
     getCellText : function(row, column) {
       var notary = notaries[row];
-      // var notary = notaryManager.getNotaryAtIndex(row);
 
       if      (column.id == "notaryHost")     return notary.getHost();
       else if (column.id == "notaryHTTPPort") return notary.getHTTPPort();
@@ -102,11 +131,9 @@ function update() {
 
     getCellValue: function(row, col) {
       return notaries[row].getEnabled();
-      // return notaryManager.getNotaryAtIndex(row).getEnabled();
     },
 
     setCellValue: function(row, col, val) {
-      // notaryManager.getNotaryAtIndex(row).setEnabled(val == "true");
       notaries[row].setEnabled(val == "true");
       update();
     },
@@ -125,36 +152,6 @@ function update() {
     getCellProperties: function(row,col,props){},  
     getColumnProperties: function(colid,col,props){}  
   };    
-
-  var certificateCache = convergence.getNativeCertificateCache();
-  cachedCerts = certificateCache.fetchAll();
-  certificateCache.close();
-  var cacheTree = document.getElementById("cacheTree");
-  cacheTree.view = {
-    rowCount: cachedCerts.length,
-
-    getCellText : function(row, column) {
-      var cachedCert = cachedCerts[row];
-
-      if      (column.id == "cacheLocation")  return cachedCert.location;
-      else if (column.id == "cacheFingerprint") return cachedCert.fingerprint;
-      else if (column.id == "cacheTimestamp")  return formatDate(cachedCert.timestamp);
-    },
-
-    setTree: function(treebox){this.treebox = treebox; },
-    isContainer: function(row){return false;},
-    isSeparator: function(row){ return false; },
-    isSorted: function(){ return false; },
-    isEditable: function(row, column) {
-      if (column.id == "notaryEnabled") return true;
-      else                              return false;
-    },
-    getLevel: function(row){ return 0; },
-    getImageSrc: function(row,col){ return null; },
-    getRowProperties: function(row,props){},
-    getCellProperties: function(row,col,props){},
-    getColumnProperties: function(colid,col,props){}
-  };
 }
 
 function issuePreferencesChangedNotification() {  
@@ -162,19 +159,20 @@ function issuePreferencesChangedNotification() {
 }
 
 function onRemoveCertificate() {
-  var tree = document.getElementById("cacheTree");
-  var id = cachedCerts[tree.currentIndex].id;
+  var tree             = document.getElementById("cacheTree");
+  var id               = cachedCerts[tree.currentIndex].id;
   var certificateCache = convergence.getNativeCertificateCache();
+
   certificateCache.deleteCertificate(id);
   certificateCache.close();
-  update();
+  updateCacheSettings();
 }
 
 function onClearCache() {
   var certificateCache = convergence.getNativeCertificateCache();
   certificateCache.clearCache();
   certificateCache.close();
-  update();
+  updateCacheSettings();
 }
 
 function onAddCertificate() {
@@ -185,23 +183,24 @@ function onAddCertificate() {
     var certificateCache = convergence.getNativeCertificateCache();
     certificateCache.cacheFingerprint(retVal.fingerprint.host, retVal.fingerprint.port, retVal.fingerprint.fingerprint);
     certificateCache.close();
-    update();
+    updateCacheSettings();
   }
 }
 
 function formatDate(date) {
-  var year = date.getFullYear();
+  var year  = date.getFullYear();
   var month = date.getMonth()+1;
-  var dom = date.getDate();
-  var hour = date.getHours();
-  var min = date.getMinutes();
-  var sec = date.getSeconds();
+  var dom   = date.getDate();
+  var hour  = date.getHours();
+  var min   = date.getMinutes();
+  var sec   = date.getSeconds();
 
   if (month < 10) month = "0" + month;
-  if (dom < 10) dom = "0" + dom;
-  if (hour < 10) hour = "0" + hour;
-  if (min < 10) min = "0" + min;
-  if (sec < 10) sec = "0" + sec;
+  if (dom < 10)   dom   = "0" + dom;
+  if (hour < 10)  hour  = "0" + hour;
+  if (min < 10)   min   = "0" + min;
+  if (sec < 10)   sec   = "0" + sec;
+
   return year + "-" + month + "-" + dom + " " + hour + ":" + min + ":" + sec;
 }
 

885ebcf015c690ab69585ac77004c72d5d80b12c
Mike Kazantsev <mk.fraggod@gmail.com>
2013-04-25 02:01:08 +0600

    server.NotaryResponse: add Content-Length header

--- server/convergence/NotaryResponse.py
+++ server/convergence/NotaryResponse.py
@@ -40,9 +40,6 @@ class NotaryResponse:
         return json.dumps(response)
 
     def sendResponse(self, code, recordRows):
-        self.request.setHeader("Content-Type", "application/json")
-        self.request.setResponseCode(code)
-
         fingerprintList = []
 
         if recordRows is not None:
@@ -52,9 +49,12 @@ class NotaryResponse:
                                'timestamp' : timestamp }
 
                 fingerprintList.append(fingerprint)
+        result = self.signResponse({'fingerprintList' : fingerprintList})
 
-        result = {'fingerprintList' : fingerprintList}
+        self.request.setHeader('Content-Type', 'application/json')
+        self.request.setHeader('Content-Length', str(len(result)))
+        self.request.setResponseCode(code)
 
-        self.request.write(self.signResponse(result))
+        self.request.write(result)
         self.request.finish()        
         
